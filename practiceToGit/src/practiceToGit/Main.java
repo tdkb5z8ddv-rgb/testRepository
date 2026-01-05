881c17baa5a0d7b0c6ebfb32fd798fc4ce415af6
T.miyazaki <tdkb5z8ddv@privaterelay.appleid.com>
2026-01-05 12:47:10 +0900

    二度目のコミット

--- practiceToGit/src/practiceToGit/Main.java
+++ practiceToGit/src/practiceToGit/Main.java
@@ -4,6 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		bird karasu=new bird("カーカー");
+		karasu.bark();
 	}
 
+}
+
+class bird{
+	String nakigoe;
+	bird(String nakigoe){
+		this.nakigoe=nakigoe;
+	}
+	void bark(){
+		System.out.println(nakigoe);
+	}
 }
